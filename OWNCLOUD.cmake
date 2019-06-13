fc030b5f981209769460c8f385d3f2f96ca4f56a
Chouagweu <50373278+Chouagweu@users.noreply.github.com>
2019-06-13 15:05:12 -0400

    Update OWNCLOUD.cmake

--- OWNCLOUD.cmake
+++ OWNCLOUD.cmake
@@ -1,16 +1,16 @@
 set( APPLICATION_NAME       "progTechSync" )
 set( APPLICATION_SHORTNAME  "progTechSync" )
 set( APPLICATION_EXECUTABLE "progtechsync" )
-set( APPLICATION_DOMAIN     "progtechvoip.ca" )
+set( APPLICATION_DOMAIN     "progtechsync.ca" )
 set( APPLICATION_VENDOR     "progTechSync" )
-set( APPLICATION_UPDATE_URL "https://progtechvoip.ca/" CACHE string "URL for updater" )
+set( APPLICATION_UPDATE_URL "https://progtechsync.ca/" CACHE string "URL for updater" )
 set( APPLICATION_ICON_NAME  "owncloud" )
 set( APPLICATION_VIRTUALFILE_SUFFIX "progtechsync" CACHE STRING "Virtual file suffix (not including the .)")
 
 set( LINUX_PACKAGE_SHORTNAME "progtechsync" )
 
 set( THEME_CLASS            "ownCloudTheme" )
-set( APPLICATION_REV_DOMAIN "ca.progtechvoip" )
+set( APPLICATION_REV_DOMAIN "ca.progtechsync" )
 set( WIN_SETUP_BITMAP_PATH  "${CMAKE_SOURCE_DIR}/admin/win/nsi" )
 
 set( MAC_INSTALLER_BACKGROUND_FILE "${CMAKE_SOURCE_DIR}/admin/osx/installer-background.png" CACHE STRING "The MacOSX installer background image")
@@ -19,5 +19,5 @@ set( MAC_INSTALLER_BACKGROUND_FILE "${CMAKE_SOURCE_DIR}/admin/osx/installer-back
 # set( APPLICATION_LICENSE    "${OEM_THEME_DIR}/license.txt )
 
 option( WITH_CRASHREPORTER "Build crashreporter" OFF )
-set( CRASHREPORTER_SUBMIT_URL "https://progtechvoip.ca" CACHE string "URL for crash reporter" )
+set( CRASHREPORTER_SUBMIT_URL "https://progtechsync.ca" CACHE string "URL for crash reporter" )
 
